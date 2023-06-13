e47e53917a5ef6c2f4c1f7998de7aef5e075b03b
Andrey G <sinezub@yandex.ru>
2023-06-13 09:25:34 +0200

    Create stub for bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+def main() -> None:
+    """Run the bot."""
+    pass
+  
+if __name__ == '__main__':
+    main()
